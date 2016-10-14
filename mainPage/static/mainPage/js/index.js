c1f25a7f2189805cfea45502e970ba505d84346b
Alexandr <slivkin.a.v@gmail.com>
2016-10-14 15:47:25 +0700

    Наружка-нск

---
+++ mainPage/static/mainPage/js/index.js
@@ -0,0 +1,6 @@
+//$("#slipToLeft_1").hover(function(){$(this).slideToggle();});
+
+TweenMax.to("#slipToLeft_1", 4, {left:'0px', repeat:0 , delay:1, ease:Elastic.easeOut});
+TweenMax.to("#slipToLeft_2", 4, {left:'0px', repeat:0 , delay:2, ease:Elastic.easeOut});
+TweenMax.to("#slipToLeft_3", 4, {left:'0px', repeat:0 , delay:3, ease:Elastic.easeOut});
+TweenMax.to("#slipToLeft_4", 4, {left:'0px', repeat:0 , delay:4, ease:Elastic.easeOut});
