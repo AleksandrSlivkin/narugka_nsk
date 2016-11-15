6176c8b3cfa079fc406d8ba1f10d2765e32138e8
Alexandr <slivkin.a.v@gmail.com>
2016-11-15 23:30:09 +0700

    изменено:      db.sqlite3 	изменено:      mainPage/static/mainPage/css/style.css 	изменено:      mainPage/static/mainPage/js/index.js 	изменено:      mainPage/templates/mainPage/base.html 	изменено:      mainPage/templates/mainPage/home.html 	изменено:      mainPage/templates/mainPage/nav_bar.html

--- mainPage/static/mainPage/js/index.js
+++ mainPage/static/mainPage/js/index.js
@@ -1,24 +1,5 @@
 $(window).ready(function () {
-    // var cs3 = $('.cs3').cs3({
-    //     pagination: {
-    //         container: '.cs3-9 .cs3-pagination'
-    //     },
-    //     navigation: {
-    //         next: '.cs3-9 .cs3-slide-next',
-    //         prev: '.cs3-9 .cs3-slide-prev'
-    //     },
-    //
-    //     autoplay: {
-    //         enabled: true,
-    //         delay: 2000,
-    //         disableOnInteraction: true
-    //     },
-    // });
-    // cs3.params.effects = "galaxy, polaroid, bricks3d, tiles3d, blocks_h_3, explosion";
-    // cs3.calcEffects();
-
-
-    var txtContainer = $("#txtContainer"), txt;
+    var txtContainer = $(".txtContainer"), txt;
 
     function splitText(phrase) {
         $.each(phrase.split(""), function (index, val) {
@@ -39,62 +20,52 @@ $(window).ready(function () {
             transformOrigin: "50% 50% -80",
             ease: Back.easeOut
         }, 0.06, "textEffect");
-        tl.staggerTo(txt, 0.6, {rotationX: "360deg", color: "#90e500", transformOrigin: "50% 50% 10"}, 0.02);
+        tl.staggerTo(txt, 0.6, {rotationX: "360deg", color: "#808080", transformOrigin: "50% 50% 10"}, 0.02);
     }
 
     function init() {
         splitText("Наружка-НСК.рф");
         buildTimeline();
-        // TweenLite.set($("#demoBackground"), {visibility: "visible"});
     }
 
     init();
+////////////////////////////////////////////////////////////////
+    var showChar = 160;
+    var ellipsestext = "...";
+    var moretext = "читать";
+    var lesstext = "скрыть";
+    $('.more').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar - 1, content.length - showChar);
+            var html = c +
+                '<span class="moreellipses">'
+                + ellipsestext + '&nbsp;' +
+                '</span>' +
+                    '<span class="morecontent">' +
+                        '<span>'
+                            + h +
+                        '</span>' +
+                    '&nbsp;&nbsp;' + '<a href="" class="morelink">' + moretext + '</a>' +
+                '</span>';
+            $(this).html(html);
+        }
+    });
 
-    // var showChar = 200;
-    // var ellipsestext = "...";
-    // var moretext = "more";
-    // var lesstext = "less";
-    // $('.more').each(function () {
-    //     var content = $(this).html();
-    //     if (content.length > showChar) {
-    //         var c = content.substr(0, showChar);
-    //         var h = content.substr(showChar - 1, content.length - showChar);
-    //         var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-    //         $(this).html(html);
-    //     }
-    //     // if (isBottomView($(this).get(0))) {
-    //     //     TweenMax.to($(this), 2.5, {alpha: 1}, 0.6);
-    //     // }
-    // });
-    //
-    // $(".morelink").click(function () {
-    //     if ($(this).hasClass("less")) {
-    //         $(this).removeClass("less");
-    //         $(this).html(moretext);
-    //     } else {
-    //         $(this).addClass("less");
-    //         $(this).html(lesstext);
-    //     }
-    //     $(this).parent().prev().toggle();
-    //     $(this).prev().toggle();
-    //     return false;
-    // });
-
-    // function isBottomView(el) {
-    //     return el.getBoundingClientRect().bottom <= window.innerHeight + el.getBoundingClientRect().height - 50;
-    // }
-    //
-    // $(window).scroll(function () {
-    //     $('.more').each(function () {
-    //         if (isBottomView($(this).get(0))) {
-    //             TweenMax.to($(this), 2.5, {alpha: 1}, 0.6);
-    //         } else {
-    //             // TweenMax.to($(this), 4, {left: '0px', repeat: 1, delay: 1, ease: Elastic.easeOut});
-    //             TweenMax.to($(this), 2.5, {alpha: 0}, 0.6);
-    //         }
-    //     })
-    // });
-
+    $(".morelink").click(function () {
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+/////////////////////////////////////////////////////////////
     var $animation_elements = $('.animation-element');
     var $window = $(window);
 
@@ -121,7 +92,8 @@ $(window).ready(function () {
 
     $window.on('scroll resize', check_if_in_view);
     $window.trigger('scroll');
-});
+})
+;
 
 
 
